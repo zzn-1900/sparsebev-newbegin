b7b0be74aaa9320a70ae937a11f3fe609b140ba2
zzn <zzn0214@qq.com>
2026-03-31 00:04:46 +0800

    v4.0

--- SparseBEV/models/csrc/wrapper.py
+++ SparseBEV/models/csrc/wrapper.py
@@ -85,6 +85,9 @@ class MSMVSamplingC23456(torch.autograd.Function):
 
 
 def msmv_sampling(mlvl_feats, sampling_locations, scale_weights):
+    sampling_locations = sampling_locations.contiguous()
+    scale_weights = scale_weights.contiguous()
+
     if len(mlvl_feats) == 4 and MSMV_CUDA:
         return MSMVSamplingC2345.apply(*mlvl_feats, sampling_locations, scale_weights)
     elif len(mlvl_feats) == 5 and MSMV_CUDA:
--- SparseBEV/models/sparsebev_sampling.py
+++ SparseBEV/models/sparsebev_sampling.py
@@ -111,12 +111,12 @@ def sampling_4d(sample_points, mlvl_feats, scale_weights, lidar2img, image_h, im
     # reorganize the tensor to stack T and G to the batch dim for better parallelism
     sample_points_cam = sample_points_cam.reshape(B, T, Q, G, P, 1, 3)
     sample_points_cam = sample_points_cam.permute(0, 1, 3, 2, 4, 5, 6)  # [B, T, G, Q, P, 1, 3]
-    sample_points_cam = sample_points_cam.reshape(B*T*G, Q, P, 3)
+    sample_points_cam = sample_points_cam.reshape(B*T*G, Q, P, 3).contiguous()
 
     # reorganize the tensor to stack T and G to the batch dim for better parallelism
     scale_weights = scale_weights.reshape(B, Q, G, T, P, -1)
     scale_weights = scale_weights.permute(0, 2, 3, 1, 4, 5)
-    scale_weights = scale_weights.reshape(B*G*T, Q, P, -1)
+    scale_weights = scale_weights.reshape(B*G*T, Q, P, -1).contiguous()
 
     # multi-scale multi-view grid sample
     final = msmv_sampling(mlvl_feats, sample_points_cam, scale_weights)
